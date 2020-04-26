5d94faa26ee312fb675eaa929cf8aaa93f2fdd9a
Kevin O'Brien <kevinobrien93@gmail.com>
2020-04-26 18:07:21 +0100

    Main Style Page Linked To bootstrap, bg image added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import './assets/css/bootstrap.min.css';
+import './assets/css/styles.css';
 
 function App() {
   return (
@@ -88,8 +89,8 @@ function App() {
             <div className="container-fluid p-0">
                 <div className="row no-gutters">
                     <div className="col-lg-4 col-sm-6">
-                        <a className="portfolio-box" href="assets/img/portfolio/fullsize/1.jpg"
-                            ><img className="img-fluid" src="assets/img/portfolio/thumbnails/1.jpg" alt="" />
+                        <a className="portfolio-box" href="./assets/img/portfolio/fullsize/1.jpg"
+                            ><img className="img-fluid" src="./assets/img/portfolio/thumbnails/1.jpg" alt="" />
                             <div className="portfolio-box-caption">
                                 <div className="project-category text-white-50">Category</div>
                                 <div className="project-name">Project Name</div>
@@ -97,8 +98,8 @@ function App() {
                         >
                     </div>
                     <div className="col-lg-4 col-sm-6">
-                        <a className="portfolio-box" href="assets/img/portfolio/fullsize/2.jpg"
-                            ><img className="img-fluid" src="assets/img/portfolio/thumbnails/2.jpg" alt="" />
+                        <a className="portfolio-box" href="./assets/img/portfolio/fullsize/2.jpg"
+                            ><img className="img-fluid" src="./assets/img/portfolio/thumbnails/2.jpg" alt="" />
                             <div className="portfolio-box-caption">
                                 <div className="project-category text-white-50">Category</div>
                                 <div className="project-name">Project Name</div>
@@ -106,8 +107,8 @@ function App() {
                         >
                     </div>
                     <div className="col-lg-4 col-sm-6">
-                        <a className="portfolio-box" href="assets/img/portfolio/fullsize/3.jpg"
-                            ><img className="img-fluid" src="assets/img/portfolio/thumbnails/3.jpg" alt="" />
+                        <a className="portfolio-box" href="./assets/img/portfolio/fullsize/3.jpg"
+                            ><img className="img-fluid" src="./assets/img/portfolio/thumbnails/3.jpg" alt="" />
                             <div className="portfolio-box-caption">
                                 <div className="project-category text-white-50">Category</div>
                                 <div className="project-name">Project Name</div>
@@ -115,8 +116,8 @@ function App() {
                         >
                     </div>
                     <div className="col-lg-4 col-sm-6">
-                        <a className="portfolio-box" href="assets/img/portfolio/fullsize/4.jpg"
-                            ><img className="img-fluid" src="assets/img/portfolio/thumbnails/4.jpg" alt="" />
+                        <a className="portfolio-box" href="./assets/img/portfolio/fullsize/4.jpg"
+                            ><img className="img-fluid" src="./assets/img/portfolio/thumbnails/4.jpg" alt="" />
                             <div className="portfolio-box-caption">
                                 <div className="project-category text-white-50">Category</div>
                                 <div className="project-name">Project Name</div>
@@ -124,8 +125,8 @@ function App() {
                         >
                     </div>
                     <div className="col-lg-4 col-sm-6">
-                        <a className="portfolio-box" href="assets/img/portfolio/fullsize/5.jpg"
-                            ><img className="img-fluid" src="assets/img/portfolio/thumbnails/5.jpg" alt="" />
+                        <a className="portfolio-box" href="./assets/img/portfolio/fullsize/5.jpg"
+                            ><img className="img-fluid" src="./assets/img/portfolio/thumbnails/5.jpg" alt="" />
                             <div className="portfolio-box-caption">
                                 <div className="project-category text-white-50">Category</div>
                                 <div className="project-name">Project Name</div>
@@ -133,8 +134,8 @@ function App() {
                         >
                     </div>
                     <div className="col-lg-4 col-sm-6">
-                        <a className="portfolio-box" href="assets/img/portfolio/fullsize/6.jpg"
-                            ><img className="img-fluid" src="assets/img/portfolio/thumbnails/6.jpg" alt="" />
+                        <a className="portfolio-box" href="./assets/img/portfolio/fullsize/6.jpg"
+                            ><img className="img-fluid" src="./assets/img/portfolio/thumbnails/6.jpg" alt="" />
                             <div className="portfolio-box-caption p-3">
                                 <div className="project-category text-white-50">Category</div>
                                 <div className="project-name">Project Name</div>
